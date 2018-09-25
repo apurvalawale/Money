3ee503afceb307ce639f5ee001bba6f46285149e
apurvalawale <sendmenote@hotmail.com>
2018-09-26 00:47:47 +0800

    SGX url updated

--- Money/Money.py
+++ Money/Money.py
@@ -6,6 +6,7 @@ import datetime
 import requests
 import configparser
 import platform
+import matplotlib.pyplot as plt
 
 configParser = configparser.RawConfigParser()   
 configFilePath = r'Config.cfg'
@@ -15,86 +16,36 @@ msftPath2=configParser.get('Paths', 'msftPath2')
 sgxPath=configParser.get('Paths', 'sgxPath')
 saveStockDataPath=configParser.get('Paths', 'saveStockDataPath')
 
-tickers=["CY6U","A17U","AU8U","M44U"]
-for companyTicker in tickers:
-    response=urllib.request.urlopen(msftPath1+companyTicker+msftPath2)
-    content=response.read()
-    data = json.loads(content.decode("utf8"))
-    response.close()
-
-    #Number of items under Stat
-    #pprint("Length: "+str(len(data["stockEntityDetails"]["Stat"])))
-
-    #check if object exists
-    stockEntityObject=data.get("stockEntityDetails")
-    if stockEntityObject:
-        #Ticker
-        ticker=data["stockEntityDetails"]["Stat"][0]["Tkr"]
-        pprint(ticker)
-        #Stock Price
-        stockPrice=data["stockEntityDetails"]["Stat"][0]["Sp"]
-        pprint(stockPrice)
-        #Market Capital
-        marketCapital=data["stockEntityDetails"]["Stat"][0]["Mc"]
-        pprint(marketCapital)
-        #Net Income
-        netIncome=data["stockDetailsV2"]["Fh"]["In"]
-        pprint(netIncome)
-        #Shares Outstanding
-        outstandingShares=data["stockEntityDetails"]["Stat"][0]["shrsOs"]
-        pprint(outstandingShares)
-        #Book Value per Share
-        bookValuePerShare=data["stockEntityDetails"]["Stat"][0]["Bvps"]
-        pprint(bookValuePerShare)
-        #Earnings per Share
-        earningsPerShare=data["stockEntityDetails"]["Stat"][0]["Eps"]
-        pprint(earningsPerShare)
-        #Price to Earnings ratio
-        priceToEarningsRatio=data["stockEntityDetails"]["Stat"][0]["Pe"]
-        pprint(priceToEarningsRatio)
-        #Dividend Yield
-        dividendYield=data["stockEntityDetails"]["Stat"][0]["Dy"]
-        pprint(dividendYield)
-        #Forward Dividend Yield
-        forwardDividendYield=data["stockEntityDetails"]["Stat"][0]["FwdDYld"]
-        pprint(forwardDividendYield)
-        #Debt To Equity ratio
-        debtToEquityRatio=data["stockEntityDetails"]["Stat"][0]["De"]
-        pprint(debtToEquityRatio)
-
-        #Enterprise Value
-        enterpriseValue=data["shareStatistics"][0]["EpVl"]
-        pprint(enterpriseValue)
-
-        #Last Dividend Amount Paid
-        #pprint(data["stockEntityDetails"]["Stat"][0]["AmtPd"])
-        #Last Ex-Dividend Date
-        #pprint(data["stockEntityDetails"]["Stat"][0]["ExdDt"])
-        #Payout Ratio
-        payoutRatio=data["stockEntityDetails"]["Stat"][0]["PytRt"]
-        pprint(payoutRatio)
-        #Quick Ratio
-        quickRatio=data["stockEntityDetails"]["Stat"][0]["QckRt"]
-        pprint(quickRatio)
-
-        payload = {"id":ticker}
-        r = requests.post(sgxPath, data=json.dumps(payload))
+tickers=["T12","F99","Z74","A17U","M44U","CY6U","AU8U"]
+for ticker in tickers:
+    payload = {"id":ticker}
+    r = requests.post(sgxPath, data=json.dumps(payload))
+    if r.status_code==200:
         r.headers["Content-Type"]="application/json"
         data=r.json()
-        stockHighPrice=data["price"]["highPrice"]
-        #Stock Low Price
-        stockLowPrice=data["price"]["lowPrice"]
+        openPrice=data["company"]["companyInfo"]["openPrice"]
+        highPrice=data["company"]["companyInfo"]["highPrice"]
+        lowPrice=data["company"]["companyInfo"]["lowPrice"]
+        closePrice=data["company"]["companyInfo"]["closePrice"]
+        volume=data["company"]["companyInfo"]["volume"]
+        enterpriseValue=data["company"]["companyInfo"]["enterpriseValue"]
+        BVperShare=data["company"]["companyInfo"]["bvShare"]
+        eps=data["company"]["companyInfo"]["eps"]
+        netIncome=data["company"]["companyInfo"]["netIncome"]
+        netProfitMargin=data["company"]["companyInfo"]["netProfitMargin"]
+        peRatio=data["company"]["companyInfo"]["peRatio"]
+        totalDebtEquity=data["company"]["companyInfo"]["totalDebtEquity"]
 
         if platform.system()=="Windows":
-            fields=[datetime.datetime.now().strftime("%Y-%m-%d"),ticker,stockPrice,marketCapital,netIncome,outstandingShares,enterpriseValue,bookValuePerShare,earningsPerShare,priceToEarningsRatio,dividendYield,forwardDividendYield,debtToEquityRatio,payoutRatio,quickRatio,stockHighPrice,stockLowPrice]
-            with open(saveStockDataPath+companyTicker+".csv", "a",newline='') as f:
+            fields=[datetime.datetime.now().strftime("%Y-%m-%d"),ticker,openPrice,highPrice,lowPrice,closePrice,volume,enterpriseValue,BVperShare,eps,netIncome,netProfitMargin,peRatio,totalDebtEquity]
+            with open(saveStockDataPath+ticker+".csv", "a",newline='') as f:
                 writer = csv.writer(f)
                 writer.writerow(fields)
     else:
         if platform.system()=="Windows":        
             with open(saveStockDataPath+"log.txt", "a",newline='') as f:
                 writer = csv.writer(f)
-                fields=[datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),companyTicker,"Stock details wasn't found"]
+                fields=[datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),ticker,"Stock details wasn't found"]
                 writer.writerow(fields)
 
 
